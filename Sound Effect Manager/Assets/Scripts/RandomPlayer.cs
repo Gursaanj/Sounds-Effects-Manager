32a322ccfb2eb372ab329f933bcce0558a873ce9
Gursaanj <gursaanjresides@gmail.com>
2019-08-08 09:55:03 -0700

    Enumerated Call
    Used enumeration to call  specific sounds, to create one global PlaySound Function

---
+++ Sound Effect Manager/Assets/Scripts/RandomPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlayer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            SoundEffectsManager.Instance.PlaySound("fire");
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            SoundEffectsManager.Instance.PlaySound("alternate");
+        }
+    }
+}
--- Sound Effect Manager/Assets/Scripts/SoundEffectsManager.cs
+++ Sound Effect Manager/Assets/Scripts/SoundEffectsManager.cs
@@ -10,6 +10,14 @@ public class SoundEffectsManager : MonoBehaviour
 
     private static SoundEffectsManager m_instance;
 
+    private static AudioSource m_audioSource;
+
+    [SerializeField]
+    private AudioClip m_fireSound;
+
+    [SerializeField]
+    private AudioClip m_alternateSound;
+
     /// <summary>
     /// For everything else to be able to access the instance of Sound Effects Manager
     /// </summary>
@@ -38,8 +46,29 @@ public class SoundEffectsManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);  
     }
 
-    public static void PlaySound()
+    private void Start()
     {
-
+        m_audioSource = GetComponent<AudioSource>();
     }
+
+    /// <summary>
+    /// General sound player when passed
+    /// </summary>
+    /// <param name="m_soundClip"></param>
+    public void PlaySound(string m_soundClip)
+    {
+        switch (m_soundClip)
+        {
+            case "fire":
+                m_audioSource.PlayOneShot(m_fireSound);
+                break;
+            case "alternate":
+                m_audioSource.PlayOneShot(m_alternateSound);
+                break;
+        }
+    }
+
+    /// create different type of sound players, maybe one for collision (that takes velocity, and force of rb) - where multiple can be played once
+    /// one for constant source that will not be impacted by factors - the lerping of target spot - when a new target spot is collected
+    /// one for ui related events - pausing, unpausing, when someone scores a point?, time at certain points, the movement of ui on the pause/end scene
 }
